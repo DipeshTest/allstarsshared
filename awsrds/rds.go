eece546aef6c269adaf59404d3ecb037e2f41d53
dipesh <dipesh.response@gmail.com>
2018-05-10 22:32:29 +0530

    rds

---
+++ awsrds/rds.go
@@ -0,0 +1,122 @@
+package awsrds
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/rds"
+)
+
+func getAwsClient(accessKeyId, secretAccessKey, region string) rds.RDS {
+	awsConfig := &aws.Config{
+		Credentials: credentials.NewStaticCredentials(accessKeyId, secretAccessKey, ""),
+
+		Region: aws.String(region),
+	}
+
+	newSession := session.New(awsConfig)
+
+	rdsClient := rds.New(newSession)
+
+	return *rdsClient
+}
+
+func StartRdsInstance(accessKeyId, secretAccessKey, region string, startDbInstnceRequest *rds.StartDBInstanceInput) (int, string) {
+	awsClient := getAwsClient(accessKeyId, secretAccessKey, region)
+
+	result, err := awsClient.StartDBInstance(startDbInstnceRequest)
+
+	if err != nil {
+
+		if aerr, ok := err.(awserr.RequestFailure); ok {
+			fmt.Println(aerr.Error())
+			return aerr.StatusCode(), aerr.Error()
+
+		}
+
+	}
+	resp, _ := json.Marshal(result)
+	return 200, string(resp)
+
+}
+
+func StopRdsInstance(accessKeyId, secretAccessKey, region string, stopDbInstnceRequest *rds.StopDBInstanceInput) (int, string) {
+	awsClient := getAwsClient(accessKeyId, secretAccessKey, region)
+
+	result, err := awsClient.StopDBInstance(stopDbInstnceRequest)
+
+	if err != nil {
+
+		if aerr, ok := err.(awserr.RequestFailure); ok {
+			fmt.Println(aerr.Error())
+			return aerr.StatusCode(), aerr.Error()
+
+		}
+
+	}
+	resp, _ := json.Marshal(result)
+	return 200, string(resp)
+
+}
+
+func RebootRdsInstance(accessKeyId, secretAccessKey, region string, rebootDbInstnceRequest *rds.RebootDBInstanceInput) (int, string) {
+	awsClient := getAwsClient(accessKeyId, secretAccessKey, region)
+
+	result, err := awsClient.RebootDBInstance(rebootDbInstnceRequest)
+
+	if err != nil {
+
+		if aerr, ok := err.(awserr.RequestFailure); ok {
+			fmt.Println(aerr.Error())
+			return aerr.StatusCode(), aerr.Error()
+
+		}
+
+	}
+	resp, _ := json.Marshal(result)
+	return 200, string(resp)
+
+}
+
+func DeleteRdsInstance(accessKeyId, secretAccessKey, region string, deleteDbInstnceRequest *rds.DeleteDBInstanceInput) (int, string) {
+	awsClient := getAwsClient(accessKeyId, secretAccessKey, region)
+
+	result, err := awsClient.DeleteDBInstance(deleteDbInstnceRequest)
+
+	if err != nil {
+
+		if aerr, ok := err.(awserr.RequestFailure); ok {
+			fmt.Println(aerr.Error())
+			return aerr.StatusCode(), aerr.Error()
+
+		}
+
+	}
+	resp, _ := json.Marshal(result)
+	return 200, string(resp)
+
+}
+
+func CreateRdsInstance(accessKeyId, secretAccessKey, region string, createDbInstnceRequest *rds.CreateDBInstanceInput) (int, string) {
+
+	awsClient := getAwsClient(accessKeyId, secretAccessKey, region)
+
+	result, err := awsClient.CreateDBInstance(createDbInstnceRequest)
+
+	if err != nil {
+
+		if aerr, ok := err.(awserr.RequestFailure); ok {
+			fmt.Println(aerr.Error())
+			return aerr.StatusCode(), aerr.Error()
+
+		}
+
+	}
+	resp, _ := json.Marshal(result)
+	return 200, string(resp)
+
+}

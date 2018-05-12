05cb6c8927027ae4399d14ad597e584f66a143f1
JayDShah <jashah@tibco.com>
2018-05-12 18:24:48 +0530

    removed fmt.printlns, removed commented code

--- gdrive/gdrive.go
+++ gdrive/gdrive.go
@@ -1,7 +1,6 @@
 package gdrive
 
 import (
-	"fmt"
 	"mime"
 	"net/http"
 	"os"
@@ -22,11 +21,6 @@ func getHTTPClient(token string, timeout string) *http.Client {
 	tok := oauth2.Token{}
 	tok.AccessToken = token
 	config := oauth2.Config{}
-
-	//	s, _ := time.ParseDuration(timeout + "s")
-	//ctx, close := context.WithTimeout(context.Background(), 5*time.Second)
-	//fmt.Println(ctx.Value(key))
-	//	defer close()
 	return config.Client(context.Background(), &tok)
 }
 
@@ -45,17 +39,12 @@ func DeleteFile(fileId, token string, timeout string) (int, string) {
 }
 func CreateFile(token, filefullpath, emailAddr, role string, sendNotification bool, timeout string) (int, string) {
 	f, err := os.Open(filefullpath)
-
 	_, name := filepath.Split(filefullpath)
-
 	defer f.Close()
 	if err != nil {
 		return 101, err.Error()
 
 	} else {
-		// if len(strings.TrimSpace(upoloadFileName)) == 0 {
-		// 	upoloadFileName = f.Name()
-		// }
 		srv, err := drive.New(getHTTPClient(token, timeout))
 		ext := filepath.Ext(f.Name())
 		baseMimeType := mime.TypeByExtension(ext)
@@ -69,7 +58,6 @@ func CreateFile(token, filefullpath, emailAddr, role string, sendNotification bo
 		ctx, close := context.WithTimeout(context.Background(), s)
 		defer close()
 		res, err := srv.Files.Create(file).Context(ctx).Media(f, googleapi.ContentType(baseMimeType)).Do()
-		fmt.Println("error ", reflect.TypeOf(err))
 		if err != nil {
 
 			switch reflect.TypeOf(err).String() {
@@ -111,8 +99,6 @@ func CreateFile(token, filefullpath, emailAddr, role string, sendNotification bo
 func ListFile(token, fileName, orderBy string, pageSize int64, pageToken string, timeout string) (int, string, int, string) {
 
 	srv, err := drive.New(getHTTPClient(token, timeout))
-
-	// if len(strings.TrimSpace(fileName)) == 0 {
 	var searchString = ""
 	if len(strings.TrimSpace(fileName)) != 0 {
 		searchString = "name=\"" + fileName + "\""
@@ -121,8 +107,6 @@ func ListFile(token, fileName, orderBy string, pageSize int64, pageToken string,
 	ctx, close := context.WithTimeout(context.Background(), s)
 	defer close()
 	resp, err := srv.Files.List().Context(ctx).PageSize(pageSize).PageToken(pageToken).Fields("kind, nextPageToken , files(id,mimeType,name,webContentLink)").Q(searchString).OrderBy(orderBy).Do()
-	fmt.Println()
-
 	if err != nil {
 		a := err.(*googleapi.Error)
 		return a.Code, a.Message, 0, ""
@@ -131,8 +115,5 @@ func ListFile(token, fileName, orderBy string, pageSize int64, pageToken string,
 
 		return resp.HTTPStatusCode, string(responseBody), len(resp.Files), resp.NextPageToken
 	}
-
 	return 1000, "", 0, ""
-	// }
-
 }
